aa38ba9507a44cd9cd9aee871b2e16499dd34433
VDG-code <61297040+VDG-code@users.noreply.github.com>
2020-03-23 21:13:33 +0100

    Delete project.py

--- project.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df=pd.read_csv('https://cocl.us/datascience_survey_data')
-df.head()
-df.columns = ['Topics','Very Interested','Somewhat Interested','Not Interested']
-df.head()
